f1da1c6d660d38ae34409f335ef273881ab0cac2
bak yeong uk <yeonguk0517@gmail.com>
2024-12-19 11:52:06 +0900

    장바구니 버튼 클릭 모달 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import BookList from '../components/home/BookList';
+// import CartModal from '../components/home/CartModal';
 
 const Home = () => {
   return (
     <Main>
+      {/* <CartModal /> */}
       <BookList />
     </Main>
   );
