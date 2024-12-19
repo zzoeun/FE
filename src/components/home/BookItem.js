4ccf47b2ae5508ea11271f23315c86cc12040474
bak yeong uk <yeonguk0517@gmail.com>
2024-12-19 10:12:43 +0900

    장바구니 버튼  이동 기능 삭제

--- src/components/home/BookItem.js
+++ src/components/home/BookItem.js
@@ -32,7 +32,7 @@ function BookItem({ book }) {
               />
             </svg>
           </Button>
-          <Button onClick={() => navigate('/cart')}>
+          <Button>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
