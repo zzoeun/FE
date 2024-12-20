9d8be57d47c30a25117ffdd4191cac5e61d58493
bak yeong uk <yeonguk0517@gmail.com>
2024-12-20 12:22:33 +0900

    너비 변경 & 정렬 변경

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,16 +7,18 @@ const Navbar = () => {
 
   return (
     <Nav>
-      <Dropdown />
+      <Wrapper>
+        <Dropdown />
 
-      <LocationList>
-        <LocationItem>
-          <LocationButton onClick={() => navigate('/login')}>로그인</LocationButton>
-        </LocationItem>
-        <LocationItem>
-          <LocationButton onClick={() => navigate('/signup')}>회원가입</LocationButton>
-        </LocationItem>
-      </LocationList>
+        <LocationList>
+          <LocationItem>
+            <LocationButton onClick={() => navigate('/login')}>로그인</LocationButton>
+          </LocationItem>
+          <LocationItem>
+            <LocationButton onClick={() => navigate('/signup')}>회원가입</LocationButton>
+          </LocationItem>
+        </LocationList>
+      </Wrapper>
     </Nav>
   );
 };
@@ -27,9 +29,16 @@ const Nav = styled.nav`
   height: 50px;
   border-top: 1px solid #e0e0e0;
   border-bottom: 1px solid #e0e0e0;
-  padding: 0 30px;
+`;
+
+const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  width: 1200px;
+  height: 100%;
+  padding: 0 30px 0 20px;
+  margin: 0 auto;
 `;
 
 const LocationList = styled.ul`
@@ -38,7 +47,6 @@ const LocationList = styled.ul`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-left: auto;
 `;
 
 const LocationItem = styled.li`
