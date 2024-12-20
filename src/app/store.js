127cc8545045b71bd8436c4194d17ed2f8a6db89
bak yeong uk <yeonguk0517@gmail.com>
2024-12-20 12:04:02 +0900

    dropdownSlice 추가

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookSliceReducer from '../features/book/bookSlice';
 import modalSliceReducer from '../features/book/modalSlice';
+import dropdownSliceReducer from '../features/book/dropdownSlice';
 
 export default configureStore({
   reducer: {
     books: bookSliceReducer,
     modal: modalSliceReducer,
+    dropdown: dropdownSliceReducer,
   },
 });
