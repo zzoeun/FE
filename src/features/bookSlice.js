fda69aa0750a05fe3a979b2d209e55e4cba98a2e
bak yeong uk <yeonguk0517@gmail.com>
2024-12-23 16:38:58 +0900

    서버로부터 데이터 요청 코드 기능 구현

--- src/features/bookSlice.js
+++ src/features/bookSlice.js
@@ -1,9 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
-import data from '../data';
+// import data from '../data';
 
 export const bookSlice = createSlice({
   name: 'books',
-  initialState: data,
+  initialState: [],
+  reducers: {
+    addBooks: (state, action) => {
+      state.push(...action.payload);
+    },
+  },
 });
 
+export const { addBooks } = bookSlice.actions;
 export default bookSlice.reducer;
