61bd3fe99f59fa40575333c667d10aeecc433367
bak yeong uk <yeonguk0517@gmail.com>
2024-12-23 17:51:08 +0900

    로고 추가

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 import Navbar from './Navbar';
+import logoImg from '../../assets/images/logo.png';
+import { useNavigate } from 'react-router';
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <HeaderArea>
-      <div></div>
+      <Logo>
+        <LogoImageBox onClick={() => navigate('/')}>
+          <img src={logoImg} alt='logo' />
+        </LogoImageBox>
+      </Logo>
       <Navbar />
     </HeaderArea>
   );
@@ -20,6 +28,23 @@ const HeaderArea = styled.header`
   top: 0;
   background: #fff;
   z-index: 5;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoImageBox = styled.div`
+  width: 500px;
+  cursor: pointer;
+
+  & img {
+    width: 100%;
+    height: 250px;
+    display: block;
+  }
 `;
 
 export default Header;
