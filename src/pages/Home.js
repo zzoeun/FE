00fdaa92c24bb1ff476023105b01af45c8b3e717
bak yeong uk <yeonguk0517@gmail.com>
2024-12-26 16:07:04 +0900

    모달 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,40 @@
 import styled from 'styled-components';
 import BookList from '../components/home/BookList';
 import Modal from '../components/modal/Modal';
-import { useSelector } from 'react-redux';
+import ModalContent from '../components/modal/ModalContent';
+import { useState } from 'react';
+import Button from '../components/modal/ModalButton';
+import ModalButtons from '../components/modal/ModalButtons';
+import { useNavigate } from 'react-router';
 
 const Home = () => {
-  const modal = useSelector((state) => state.modal);
+  const navigate = useNavigate();
+  const [modal, setModal] = useState(false);
+
+  const handleCloseModal = () => {
+    setModal(false);
+  };
+
+  const handleMoveCartPage = () => {
+    navigate('/cart');
+    setModal(false);
+  };
 
   return (
     <Main>
-      {modal && <Modal />}
-      <BookList />
+      {modal && (
+        <Modal>
+          <ModalContent>
+            <p>장바구니에 상품을 담았습니다.</p>
+            <p>장바구니로 바로 이동하시겠습니까?</p>
+          </ModalContent>
+          <ModalButtons>
+            <Button onClick={handleMoveCartPage}>확인</Button>
+            <Button onClick={handleCloseModal}>취소</Button>
+          </ModalButtons>
+        </Modal>
+      )}
+      <BookList setModal={setModal} />
     </Main>
   );
 };
